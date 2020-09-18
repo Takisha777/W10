ca1ba5be637a011ded3a1052036e0ba0c11d0d54
Takisha <alexey.kovtun.od@gmail.com>
2020-09-18 17:49:19 +0300

    finish

--- task.js
+++ task.js
@@ -5,3 +5,26 @@
 // Уровень 0 - 50 (включительно) - должен иметь зеленый цвет. От 50 до 75(включительно) -
 // желтый. От 75 - 100 - красный. Добавить проверку для поля ввода. Значение должно быть
 // числом и должно входить в предел 0 - 100.
+
+window.onload = function () {
+    let range = document.querySelector("#range");
+    let mask = document.querySelector(".mask");
+    let text = document.querySelector("#text");
+    range.addEventListener("mousedown", function () {
+        this.addEventListener("mousemove", move);
+    });
+    function move() {
+        mask.style.height = (100-range.value) + 'px';
+        document.getElementById("text").value = range.value;
+    }
+
+    text.addEventListener("keyup", function () {
+        if (text.value > 100){
+            text.value = 100;
+        }
+        mask.style.height = (100 - text.value) + 'px';
+        document.getElementById("range").value = text.value;
+    })
+};
+
+
